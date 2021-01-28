f23d562138ce85574eaaa16b47dbb95a91294627
ryouki.tanaka <key_dogma@icloud.com>
2021-01-28 18:22:22 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 田中陵暉 on 2021/01/28.
+//  Copyright © 2021 ryouki.tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
